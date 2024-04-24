80dc16adacd9b1b84e4f8a23d7da073f2a167a38
Andrew <lordmungus@gmail.com>
2024-04-24 22:47:00 +0200

    fix: fixed ai waypoints

--- scripts/Game/AI/Components/PAP_FollowComponent.c
+++ scripts/Game/AI/Components/PAP_FollowComponent.c
@@ -4,6 +4,17 @@ modded class DAD_FollowComponent
 	const protected string GETIN_WP_PATTERN = "Waypoint_GetIn";
 	const protected string WAIT_WP_PATTERN = "Waypoint_Wait";
 	
+	
+	void ResetWaypoints()
+	{
+		AIGroup ai = GetAI();
+		if (!ai) return;
+		while (ai.GetCurrentWaypoint()) 
+		{
+			ai.CompleteWaypoint(ai.GetCurrentWaypoint());
+		}
+	}
+	
 	override void StopFollowing()
 	{
 		ResetWaypoints();
@@ -14,49 +25,87 @@ modded class DAD_FollowComponent
 	
 	override bool IsFollowing()
 	{
-		bool isFollowing;
-		SCR_ChimeraCharacter owner = SCR_ChimeraCharacter.Cast(GetOwner());
-
-		AIWaypoint wp = GetAI().GetCurrentWaypoint();
-		if (!wp || !SCR_AIDamageHandling.IsAlive(m_User))
+		
+		if (m_User && !SCR_AIDamageHandling.IsAlive(m_User)) 
 		{
-			isFollowing = false;	
+			Print("PAP_FollowComponent:IsFollowing() | User is dead... units unfollowing");
+			return false;
 		}
+		
+		AIGroup aiGroup = GetAI();
+		if (!aiGroup)
+		{
+			Print("PAP_FollowComponent:IsFollowing() | Ai group not found");
+			return false;
+		} 
+		
+		ref array<AIWaypoint> aiWaypoints = {};
+		aiGroup.GetWaypoints(aiWaypoints);
+		
+		if (!aiWaypoints || aiWaypoints.IsEmpty()) return false;
 		else
 		{
-			EntityPrefabData prefab = wp.GetPrefabData();
-			isFollowing = prefab && prefab.GetPrefabName().Contains(FOLLOW_WP_PATTERN);
+			foreach (AIWaypoint wp : aiWaypoints)
+			{
+				EntityPrefabData prefab = wp.GetPrefabData();
+				if (prefab && prefab.GetPrefabName().Contains(FOLLOW_WP_PATTERN))
+				{
+					Print("PAP_FollowComponent:IsFollowing() | At least one unit is still following");
+					return true;
+				} 
+			}
 		}
-		return isFollowing;
+		Print("PAP_FollowComponent:IsFollowing() | Units assumed to not be following");
+		return false;
+	}
+	
+	override void UpdateIsFollowing()
+	{
+		AIGroup aiGroup = GetAI();
+		
+		if (!aiGroup)
+		{	
+			Print("PAP_FollowComponent:UpdateIsFollowing() | Ai group not found");
+			return;
+		}
+		
+		bool isFollowing = IsFollowing();
+		ref array<AIAgent> agents = {};
+		aiGroup.GetAgents(agents);
+		foreach (AIAgent agent : agents)
+		{
+			SCR_ChimeraCharacter char = SCR_ChimeraCharacter.Cast(agent.GetControlledEntity());
+			char.SetIsFollowing(isFollowing);
+		}
 	}
 	
 	override void Update() {
-		Print("DAD_FollowComponent: Updating");
+		Print("PAP_FollowComponent:Update() | Updating...");
 				
 		UpdateIsFollowing();
 
-		if (!IsFollowing()) return;
-			
-		if (!SCR_AIDamageHandling.IsAlive(m_User))
+		if (!IsFollowing())
 		{
-			Print("DAD_FollowComponent: Player is not alive");
+			Print("PAP_FollowComponent:Update() | No units following... ending queue");
+			return;
+		}
+			
+		if (m_User && !SCR_AIDamageHandling.IsAlive(m_User))
+		{
+			Print("PAP_FollowComponent:Update() | Player is not alive... ending queue");
 			StopFollowing();
 			return;
 		}
 
-
-		Print("Following!");		
 		AIGroup ai = GetAI();
-		if (!ai) 
-		{
-			QueueUpdate();
-			return;
-		}
+		if (!ai) return;
 
-		vector playerOrigin = m_User.GetOrigin();
+		Print("PAP_FollowComponent:Update() | Units are following");		
 
 		ref array<AIAgent> agents = {};
 		ai.GetAgents(agents);
+		
+		vector playerOrigin = m_User.GetOrigin();
 
 		if (m_User.IsInVehicle())
 		{
@@ -66,38 +115,43 @@ modded class DAD_FollowComponent
 				ChimeraCharacter character = ChimeraCharacter.Cast(a.GetControlledEntity());
 				if (character && !character.IsInVehicle())
 				{
-					Print("At least one follower not embarked!");
+					Print("PAP_FollowComponent:Update() | At least one follower not embarked!");
 					groupNeedsToGetInVehicle = true;
 					break;
 				}
 			}
 			if (groupNeedsToGetInVehicle)
 			{
-				Print("Adding waypoints to mount vehicle!");
-				AIWaypoint getInWaypoint = AIWaypoint.Cast(SpawnHelpers.SpawnEntity(
-					Resource.Load("{0A2A37B4A56D74DF}PrefabsEditable/Auto/AI/Waypoints/E_AIWaypoint_GetInNearest.et"),
-					playerOrigin
-				));
-				AIWaypoint stayInCar = AIWaypoint.Cast(SpawnHelpers.SpawnEntity(
-					Resource.Load("{90E783A161383314}PrefabsEditable/Auto/AI/Waypoints/E_AIWaypoint_Wait.et"),
-					playerOrigin
-				));
-				ai.AddWaypointAt(stayInCar, 0);
-				ai.AddWaypointAt(getInWaypoint, 0);
+				Print("PAP_FollowComponent:Update() | Adding waypoints to mount vehicle if required!");
+				if (!ai.GetCurrentWaypoint().GetPrefabData().GetPrefabName().Contains(GETIN_WP_PATTERN) &&
+				 	!ai.GetCurrentWaypoint().GetPrefabData().GetPrefabName().Contains(WAIT_WP_PATTERN))
+				{
+					AIWaypoint getInWaypoint = AIWaypoint.Cast(SpawnHelpers.SpawnEntity(
+						Resource.Load("{0A2A37B4A56D74DF}PrefabsEditable/Auto/AI/Waypoints/E_AIWaypoint_GetInNearest.et"),
+						playerOrigin
+					));
+					AIWaypoint stayInCar = AIWaypoint.Cast(SpawnHelpers.SpawnEntity(
+						Resource.Load("{90E783A161383314}PrefabsEditable/Auto/AI/Waypoints/E_AIWaypoint_Wait.et"),
+						playerOrigin
+					));
+					ai.AddWaypointAt(stayInCar, 0);
+					ai.AddWaypointAt(getInWaypoint, 0);
+				}
 			}
 			
 		}
 		else
 		{
+			Print("PAP_FollowComponent:Update() | User not in a vehicle");
 			while (ai.GetCurrentWaypoint().GetPrefabData().GetPrefabName().Contains(GETIN_WP_PATTERN))
 			{
-				Print("Removing Waypoint_GetIn waypoint");
+				Print("PAP_FollowComponent:Update() | Removing Waypoint_GetIn waypoint");
 				ai.RemoveWaypointAt(0);
 			}
 			
 			while (ai.GetCurrentWaypoint().GetPrefabData().GetPrefabName().Contains(WAIT_WP_PATTERN))
 			{
-				Print("Removing Waypoint_Wait waypoint");
+				Print("PAP_FollowComponent:Update() | Removing Waypoint_Wait waypoint");
 				ai.RemoveWaypointAt(0);
 			}
 		}
@@ -106,18 +160,10 @@ modded class DAD_FollowComponent
 
 	}
 	
-	void ResetWaypoints()
-	{
-		AIGroup ai = GetAI();
-		while (ai.GetCurrentWaypoint()) 
-		{
-			ai.CompleteWaypoint(ai.GetCurrentWaypoint());
-		}
-	}
+	
 	
 	void QueueUpdate()
 	{
-		Print("Queuing Update()");
 		GetGame().GetCallqueue().CallLater(Update, 2 * 1000, false);
 	}
 	
