d983b91983f30146338145f97b2ab847758c75b0
Andrew <lordmungus@gmail.com>
2024-04-29 22:27:24 +0200

    feat:
    - changed russian groups to modern variant
    
    fix:
    
    - upped price of gl squad to 500
    - removed some ai defend behaviour overrides
    - increased size of completion radius of GetIn_Nearest order to 10m

--- scripts/Game/Components/Spawner/CatalogSpawner/SCR_PAP_DefenderSpawnerComponent.c
+++
@@ -1,9 +0,0 @@
-modded class SCR_DefenderSpawnerComponentClass
-{
-	[Attribute(defvalue: "{531EC45063C1F57B}Prefabs/AI/Waypoints/AIWaypoint_Wait.et", UIWidgets.ResourceNamePicker, desc: "Default waypoint prefab", "et", category: "Defender Spawner")]
-	protected ResourceName m_sDefaultWaypointPrefab;
-	
-	[Attribute(defvalue :"StaggeredColumn", UIWidgets.Auto, desc: "Group formation created on rally point. Available formations are found on SCR_AIWorld entity", category: "Defender Spawner")]
-	protected string m_sExitFormation;
-
-}
